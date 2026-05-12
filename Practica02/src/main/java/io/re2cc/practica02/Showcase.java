d3f11c8bab0599f3e6e959cce411b7376b763742
re2cc <42478705+re2cc@users.noreply.github.com>
2026-05-11 21:23:39 -0600

    Practica 2: Showcase class

--- Practica02/src/main/java/io/re2cc/practica02/Showcase.java
+++ Practica02/src/main/java/io/re2cc/practica02/Showcase.java
@@ -1,4 +1,29 @@
 package io.re2cc.practica02;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class Showcase {
+    ArrayList<Collectible> storedCollectibles = new ArrayList<>();
+
+    Showcase() {}
+
+    void addCollectible(Collectible collectible) {
+        storedCollectibles.add(collectible);
+    }
+
+    Optional<Collectible> searchCollectible(String name) {
+        for (Collectible collectible : storedCollectibles) {
+            if (collectible.name.equals(name)) {
+                return Optional.of(collectible);
+            }
+        }
+        return Optional.empty();
+    }
+
+    void showCollectibles() {
+        for (Collectible collectible : storedCollectibles) {
+            IO.println(collectible.name);
+        }
+    }
 }
