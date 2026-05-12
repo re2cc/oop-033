d6c5ab16d4927dd5a6fb86df4fcee231a0f493f0
re2cc <42478705+re2cc@users.noreply.github.com>
2026-05-11 20:05:09 -0600

    Practica 2: Add Collectible constructors

---
+++ Practica02/BITACORA_IA.md
@@ -0,0 +1,4 @@
+- Is chaining constructors worth it?
+
+En pocas palabras si, tiene sentido el codigo se ve mas limpio. Aunque por alguna razón te fuerza a que lo hagas en la
+primera linea del constructor.
--- Practica02/src/main/java/io/re2cc/practica02/Collectible.java
+++ Practica02/src/main/java/io/re2cc/practica02/Collectible.java
@@ -1,4 +1,50 @@
 package io.re2cc.practica02;
 
 public class Collectible {
+    String name;
+    float uvaThreshold;
+    float uvbThreshold;
+    float temperatureThreshold;
+    float humidityThreshold;
+
+    Collectible(String name, float uvaThreshold,  float uvbThreshold, float temperatureThreshold, float humidityThreshold) {
+        this.name = name;
+        this.uvaThreshold = uvaThreshold;
+        this.uvbThreshold = uvbThreshold;
+        this.temperatureThreshold = temperatureThreshold;
+        this.humidityThreshold = humidityThreshold;
+    }
+
+    Collectible(String name) {
+        this(name, 0.5f, 0.5f, 0.5f, 0.5f);
+    }
+
+    Collectible(String name, String material) {
+        this(name);
+
+        switch (material) {
+            case "PVC":
+                this.uvaThreshold = 0.1f;
+                this.uvbThreshold = 0.1f;
+                this.temperatureThreshold = 0.1f;
+                this.humidityThreshold = 0.1f;
+                break;
+            case "Paper":
+                this.uvaThreshold = 0.2f;
+                this.uvbThreshold = 0.2f;
+                this.temperatureThreshold = 0.2f;
+                this.humidityThreshold = 0.2f;
+                break;
+            case "Oil":
+                this.uvaThreshold = 0.3f;
+                this.uvbThreshold = 0.3f;
+                this.temperatureThreshold = 0.3f;
+                this.humidityThreshold = 0.3f;
+                break;
+            default:
+                IO.println("Advertencia, el material " + material + " no esta registrado. Se usaran valores predeterminados");
+                break;
+        }
+    }
+
 }
