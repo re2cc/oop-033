255dd80c25dbf7a0478fd2f7c61da4122a22314f
re2cc <42478705+re2cc@users.noreply.github.com>
2026-05-14 12:35:34 -0600

    Practica 4: Copy all the files from the last practice but change the package name to model

---
+++ Practica04/src/main/java/io/re2cc/model/Showcase.java
@@ -0,0 +1,34 @@
+package io.re2cc.model;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class Showcase {
+    private ArrayList<Collectible> storedCollectibles = new ArrayList<>();
+    protected int capacity = 5; // Default capacity, other showcase types may have a different ones
+
+    Showcase() {}
+
+    void addCollectible(Collectible collectible) {
+        if (storedCollectibles.size() < capacity) {
+            storedCollectibles.add(collectible);
+        } else {
+            IO.println("Error: There is no more space in the showcase");
+        }
+    }
+
+    Optional<Collectible> searchCollectible(String name) {
+        for (Collectible collectible : storedCollectibles) {
+            if (collectible.getName().equals(name)) {
+                return Optional.of(collectible);
+            }
+        }
+        return Optional.empty();
+    }
+
+    void showCollectibles() {
+        for (Collectible collectible : storedCollectibles) {
+            IO.println(collectible);
+        }
+    }
+}
