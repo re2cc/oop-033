f60ecc461124566c34aa3818bbff683104930b5f
re2cc <42478705+re2cc@users.noreply.github.com>
2026-05-13 19:52:03 -0600

    Practica 3: Add some basic unit test for Collectible

---
+++ Practica03/src/test/java/io/re2cc/practica03/CollectibleTest.java
@@ -0,0 +1,44 @@
+package io.re2cc.practica03;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollectibleTest {
+
+    @Test
+    void setHumidityThresholdOver100() {
+        Collectible collectible = new Collectible("Item");
+
+        assertThrows(IllegalArgumentException.class, () -> collectible.setHumidityThreshold(101.0f));
+    }
+
+    @Test
+    void setHumidityThresholdUnder100() {
+        Collectible collectible = new Collectible("Item");
+
+        assertThrows(IllegalArgumentException.class, () -> collectible.setHumidityThreshold(-1.0f));
+    }
+
+    @Test
+    void setHumidityThresholdValid() {
+        Collectible collectible = new Collectible("Item");
+
+        assertDoesNotThrow(() -> collectible.setHumidityThreshold(50.0f));
+    }
+
+    @Test
+    void setTemperatureThresholdUnderPossible() {
+        Collectible collectible = new Collectible("Item");
+
+        assertThrows(IllegalArgumentException.class, () -> collectible.setTemperatureThreshold(-300.0f));
+    }
+
+    @Test
+    void setTemperatureThresholdPossible() {
+        Collectible collectible = new Collectible("Item");
+
+        assertDoesNotThrow(() -> collectible.setTemperatureThreshold(-200.0f));
+    }
+}
