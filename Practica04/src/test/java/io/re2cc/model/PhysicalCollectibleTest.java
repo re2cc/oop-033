43d61f87dcbf7da4c19569eb751958da3b16aa02
re2cc <42478705+re2cc@users.noreply.github.com>
2026-05-14 13:01:15 -0600

    Practica 4: Fix test

--- Practica04/src/test/java/io/re2cc/model/PhysicalCollectibleTest.java
+++ Practica04/src/test/java/io/re2cc/model/PhysicalCollectibleTest.java
@@ -1,43 +1,42 @@
 package io.re2cc.model;
 
-import io.re2cc.model.Collectible;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CollectibleTest {
+class PhysicalCollectibleTest {
 
     @Test
     void setHumidityThresholdOver100() {
-        Collectible collectible = new Collectible("Item");
+        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertThrows(IllegalArgumentException.class, () -> collectible.setHumidityThreshold(101.0f));
     }
 
     @Test
     void setHumidityThresholdUnder100() {
-        Collectible collectible = new Collectible("Item");
+        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertThrows(IllegalArgumentException.class, () -> collectible.setHumidityThreshold(-1.0f));
     }
 
     @Test
     void setHumidityThresholdValid() {
-        Collectible collectible = new Collectible("Item");
+        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertDoesNotThrow(() -> collectible.setHumidityThreshold(50.0f));
     }
 
     @Test
     void setTemperatureThresholdUnderPossible() {
-        Collectible collectible = new Collectible("Item");
+        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertThrows(IllegalArgumentException.class, () -> collectible.setTemperatureThreshold(-300.0f));
     }
 
     @Test
     void setTemperatureThresholdPossible() {
-        Collectible collectible = new Collectible("Item");
+        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertDoesNotThrow(() -> collectible.setTemperatureThreshold(-200.0f));
     }
