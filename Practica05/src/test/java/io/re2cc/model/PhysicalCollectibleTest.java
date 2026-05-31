9ec770dd63fac1287ef9f2101db356162a0548cc
re2cc <42478705+re2cc@users.noreply.github.com>
2026-05-31 02:33:51 -0600

    Practica 5: Rewrite test

--- Practica05/src/test/java/io/re2cc/model/PhysicalCollectibleTest.java
+++ Practica05/src/test/java/io/re2cc/model/PhysicalCollectibleTest.java
@@ -6,37 +6,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PhysicalCollectibleTest {
 
+    private static class TestPhysicalCollectible extends PhysicalCollectible {
+        public TestPhysicalCollectible(String name, float uva, float uvb, float temp, float hum) {
+            super(name, uva, uvb, temp, hum);
+        }
+
+        @Override public float getBaseDepreciationRate() { return 0.05f; }
+        @Override public float getRiskMultiplier() { return 1.0f; }
+        @Override public boolean needsSpecialCare() { return false; }
+        @Override public String getPreservationInstructions() { return ""; }
+        @Override public double estimateValue(double basePrice) { return basePrice; }
+        @Override public double estimateValue(double basePrice, int yearsElapsed) { return basePrice; }
+        @Override public double estimateValue(double basePrice, int yearsElapsed, boolean isRare) { return basePrice; }
+    }
+
     @Test
     void setHumidityThresholdOver100() {
-        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
+        PhysicalCollectible collectible = new TestPhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertThrows(IllegalArgumentException.class, () -> collectible.setHumidityThreshold(101.0f));
     }
 
     @Test
     void setHumidityThresholdUnder100() {
-        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
+        PhysicalCollectible collectible = new TestPhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertThrows(IllegalArgumentException.class, () -> collectible.setHumidityThreshold(-1.0f));
     }
 
     @Test
     void setHumidityThresholdValid() {
-        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
+        PhysicalCollectible collectible = new TestPhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertDoesNotThrow(() -> collectible.setHumidityThreshold(50.0f));
     }
 
     @Test
     void setTemperatureThresholdUnderPossible() {
-        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
+        PhysicalCollectible collectible = new TestPhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertThrows(IllegalArgumentException.class, () -> collectible.setTemperatureThreshold(-300.0f));
     }
 
     @Test
     void setTemperatureThresholdPossible() {
-        PhysicalCollectible collectible = new PhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
+        PhysicalCollectible collectible = new TestPhysicalCollectible("Item", 0.5f, 0.5f, 0.5f, 0.5f);
 
         assertDoesNotThrow(() -> collectible.setTemperatureThreshold(-200.0f));
     }
