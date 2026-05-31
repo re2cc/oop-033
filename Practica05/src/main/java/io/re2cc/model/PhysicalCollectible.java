cbf32f1d84ee6dd61cab43450ec5f4272c3f44c3
re2cc <42478705+re2cc@users.noreply.github.com>
2026-05-31 00:21:03 -0600

    Practica 5: Implement Preservable to PhysicalCollectible and add abstract methods

--- Practica05/src/main/java/io/re2cc/model/PhysicalCollectible.java
+++ Practica05/src/main/java/io/re2cc/model/PhysicalCollectible.java
@@ -1,12 +1,13 @@
 package io.re2cc.model;
 
-public abstract class PhysicalCollectible extends Collectible {
+public abstract class PhysicalCollectible extends Collectible implements Preservable {
     private float uvaThreshold;
     private float uvbThreshold;
     private float temperatureThreshold;
     private float humidityThreshold;
 
-    public PhysicalCollectible(String name, float uvaThreshold, float uvbThreshold, float temperatureThreshold, float humidityThreshold) {
+    public PhysicalCollectible(String name, float uvaThreshold, float uvbThreshold, float temperatureThreshold,
+            float humidityThreshold) {
         super(name);
         setUvaThreshold(uvaThreshold);
         setUvbThreshold(uvbThreshold);
@@ -14,7 +15,10 @@ public abstract class PhysicalCollectible extends Collectible {
         setHumidityThreshold(humidityThreshold);
     }
 
-    public float getHumidityThreshold() { return humidityThreshold; }
+    public float getHumidityThreshold() {
+        return humidityThreshold;
+    }
+
     public void setHumidityThreshold(float humidityThreshold) {
         if (humidityThreshold < 0 || humidityThreshold > 100) { // Is percentage
             throw new IllegalArgumentException("Error: Humidity must be between 0% y 100%.");
@@ -22,23 +26,38 @@ public abstract class PhysicalCollectible extends Collectible {
         this.humidityThreshold = humidityThreshold;
     }
 
-    public float getTemperatureThreshold() { return temperatureThreshold; }
+    public float getTemperatureThreshold() {
+        return temperatureThreshold;
+    }
+
     public void setTemperatureThreshold(float temperatureThreshold) {
         if (temperatureThreshold < -273.15f) { // Less than that is less than 0 K which is impossible
-            throw new IllegalArgumentException("Error: Impossible temperature. The value must be greater than -273.15° C.");
+            throw new IllegalArgumentException(
+                    "Error: Impossible temperature. The value must be greater than -273.15° C.");
         }
         this.temperatureThreshold = temperatureThreshold;
     }
 
-    public float getUvaThreshold() { return uvaThreshold; }
-    public void setUvaThreshold(float uvaThreshold) { this.uvaThreshold = uvaThreshold; }
+    public float getUvaThreshold() {
+        return uvaThreshold;
+    }
 
-    public float getUvbThreshold() { return uvbThreshold; }
-    public void setUvbThreshold(float uvbThreshold) { this.uvbThreshold = uvbThreshold; }
+    public void setUvaThreshold(float uvaThreshold) {
+        this.uvaThreshold = uvaThreshold;
+    }
+
+    public float getUvbThreshold() {
+        return uvbThreshold;
+    }
+
+    public void setUvbThreshold(float uvbThreshold) {
+        this.uvbThreshold = uvbThreshold;
+    }
 
     @Override
     public String toString() {
-        return String.format("PhysicalCollectible[Name: %s | UVA: %.2f | UVB: %.2f | Temperature: %.2f° C | Humidity: %.2f%%]",
+        return String.format(
+                "PhysicalCollectible[Name: %s | UVA: %.2f | UVB: %.2f | Temperature: %.2f° C | Humidity: %.2f%%]",
                 getName(), uvaThreshold, uvbThreshold, temperatureThreshold, humidityThreshold);
     }
 
@@ -61,4 +80,18 @@ public abstract class PhysicalCollectible extends Collectible {
     public float getSuggestedTemperature() {
         return this.temperatureThreshold - 5;
     }
+
+    public abstract float getBaseDepreciationRate();
+
+    public abstract float getRiskMultiplier();
+
+    public float calculateMaintenanceCost(float baseValue) {
+        return baseValue * getRiskMultiplier() + (baseValue * getBaseDepreciationRate());
+    }
+
+    public abstract double estimateValue(double basePrice);
+
+    public abstract double estimateValue(double basePrice, int yearsElapsed);
+
+    public abstract double estimateValue(double basePrice, int yearsElapsed, boolean isRare);
 }
