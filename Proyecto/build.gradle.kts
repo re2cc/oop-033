2399a2d85a909e8507b5567d7f6cd9de6ff361d5
re2cc <42478705+re2cc@users.noreply.github.com>
2026-06-05 19:51:04 -0600

    Proyecto: Init

---
+++ Proyecto/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "io.re2cc"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("io.re2cc.Main.Main")
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ Proyecto/src/main/java/io/re2cc/Main/Main.java
@@ -0,0 +1,12 @@
+package io.re2cc.Main;
+
+import io.re2cc.model.*;
+import io.re2cc.controller.*;
+import io.re2cc.view.*;
+import io.re2cc.exception.*;
+
+public class Main {
+    static void main() {
+        IO.println("Test");
+    }
+}
