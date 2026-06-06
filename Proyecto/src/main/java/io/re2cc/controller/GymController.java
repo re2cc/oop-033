2fffd88ff7324f2a8d36ce6d65214b560ca4043f
re2cc <42478705+re2cc@users.noreply.github.com>
2026-06-06 17:20:52 -0600

    Proyecto: Add empty GymController controller class

---
+++ Proyecto/src/main/java/io/re2cc/controller/GymController.java
@@ -0,0 +1,45 @@
+package io.re2cc.controller;
+
+import javafx.fxml.FXML;
+
+public class GymController {
+
+    @FXML
+    private void handleSave() {}
+
+    @FXML
+    private void handleGenerateReport() {}
+
+    @FXML
+    private void handleNextDay() {}
+
+    @FXML
+    private void handleClientAdd() {}
+
+    @FXML
+    private void handleClientEdit() {}
+
+    @FXML
+    private void handleAccessEnter() {}
+
+    @FXML
+    private void handleAccessExit() {}
+
+    @FXML
+    private void handleMembershipAdd() {}
+
+    @FXML
+    private void handleMembershipEdit() {}
+
+    @FXML
+    private void handleInventoryAdd() {}
+
+    @FXML
+    private void handleInventoryEdit() {}
+
+    @FXML
+    private void handleClassAdd() {}
+
+    @FXML
+    private void handleClassEdit() {}
+}
